4a2c5f8c0a7d89b69d01ba0391307eecd9a15fab
Hin Wong <hinsolo@Hins-MacBook-Pro.local>
2020-03-30 14:09:43 -0600

    beginning

---
+++ JournalCloudKit/JournalCloudKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JournalCloudKit
+//
+//  Created by Hin Wong on 3/30/20.
+//  Copyright © 2020 Hin Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
